7b5b7b75f4de5a8a6b5b9790fef9a81d52ac062d
Alexis Hernández <alexisx16@gmail.com>
2025-09-21 02:34:59 -0600

    update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 onload = () =>{
         document.body.classList.remove("container");
+          // Reproduce la música después de 0.1s
+    setTimeout(() => {
+        const audio = document.getElementById("bg-music");
+        if (audio) {
+            audio.play().catch(err => {
+                console.log("Autoplay bloqueado por el navegador:", err);
+            });
+        }
+    }, 100); // 100 ms = 0.1s
 };
